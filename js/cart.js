50b4ff66fdeeaf51aea5a6e24778a9465ccd7815
Danil <danil.lutsiv@geniussports.com>
2022-11-27 19:18:22 +0200

    Add template

---
+++ js/cart.js
@@ -0,0 +1,92 @@
+const CARD_KEY = "v1_card";
+const cardList = document.getElementById("card");
+
+function renderCard() {
+    const list = getCardList();
+    cardList.innerHTML = "";
+
+    if (!list.length) {
+        cardList.innerHTML = "<p class='empty-cart'>Корзина порожня</p>";
+    } else {
+        cardList.innerHTML = "<a href=\"#contact\" class=\"btn\">Замовити зараз</a>";
+        list?.forEach(({id, quantity}) => {
+            const item = [...products, ...menu].flat().find((item) => item.id === id);
+
+            if (!item) {
+                return;
+            }
+
+            const tempDiv = document.createElement('div');
+            tempDiv.classList.add("cart-item");
+            tempDiv.innerHTML = `
+                <span class="fas fa-times" onclick="removeFromCard(${id})"></span>
+                <img src="${item.img}" alt="">
+                <div class="content">
+                    <h3>${item.title} (${quantity})</h3>
+                    <div class="price">${item.price.toFixed(2)}/${(item.price * quantity).toFixed(2)}</div>
+                </div>
+            `;
+
+            cardList?.insertAdjacentElement("afterbegin", tempDiv)
+        })
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    renderCard()
+});
+
+function getCardList() {
+    try {
+        const list = localStorage.getItem(CARD_KEY);
+
+        if (!list) {
+            localStorage.setItem(CARD_KEY, JSON.stringify([]))
+            return []
+        }
+
+        return JSON.parse(list)
+    } catch (e) {
+        localStorage.removeItem(CARD_KEY);
+        return []
+    }
+}
+
+function setCardList(data) {
+    try {
+        localStorage.setItem(CARD_KEY, JSON.stringify(data));
+        cartItem.classList.add('active');
+        renderCard()
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+function addToCard(id) {
+    try {
+        const card = getCardList();
+        const item = card.find((item) => item.id === id);
+
+        if (item) {
+            item.quantity += 1
+        } else {
+            card.push({
+                id,
+                quantity: 1,
+            })
+        }
+
+        setCardList(card);
+    } catch (e) {
+        console.error(4)
+    }
+}
+
+function removeFromCard(id) {
+    try {
+        const list = getCardList().filter((item) => item.id !== id);
+        setCardList(list);
+    } catch (e) {
+        console.error(4)
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,100 @@
+const menuList = document.getElementById("menu-list")
+
+menu?.forEach(({img, oldPrice, price, title, id}) => {
+    const tempDiv = document.createElement('div');
+    tempDiv.classList.add("box");
+    tempDiv.dataset.id = id;
+    tempDiv.innerHTML = `
+        <img src="${img}" alt="">
+        <h3>${title}</h3>
+        <div class="price">${price} <span>${oldPrice}</span></div>
+        <button class="btn" onclick="addToCard(${id})">Додати до кошику</button>
+    `;
+
+    menuList?.insertAdjacentElement("beforeend", tempDiv)
+})
+
+const productsList = document.getElementById("products-list")
+
+products?.forEach(({img, oldPrice, price, title, id, stars}) => {
+    const tempDiv = document.createElement('div');
+    tempDiv.classList.add("box");
+    tempDiv.dataset.id = id;
+    tempDiv.innerHTML = `
+        <div class="icons">
+            <button onclick="addToCard(${id})" class="fas fa-shopping-cart"></button>
+            <a href="#" class="fas fa-heart"></a>
+            <a href="#" class="fas fa-eye"></a>
+        </div>
+        <div class="image">
+            <img src="${img}" alt="">
+        </div>
+        <div class="content">
+            <h3>${title}</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <div class="price">${price} <span>${oldPrice}</span></div>
+        </div>
+    `;
+
+    productsList?.insertAdjacentElement("beforeend", tempDiv)
+})
+
+const reviewsList = document.getElementById("reviews-list")
+
+reviews?.forEach(({img, id, stars, desc, name}) => {
+    const tempDiv = document.createElement('div');
+    tempDiv.classList.add("box");
+    tempDiv.dataset.id = id;
+    tempDiv.innerHTML = `
+        <img src="images/quote-img.png" alt="" class="quote">
+        <p class="review-message">${desc}</p>
+        <img src="${img}" class="user" alt="">
+        <h3>${name}</h3>
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+        </div>
+    `;
+
+    reviewsList?.insertAdjacentElement("beforeend", tempDiv)
+})
+
+const blogsList = document.getElementById("blogs-list")
+
+blogs?.forEach(({img, id, desc, title, author}) => {
+    const tempDiv = document.createElement('div');
+    tempDiv.classList.add("box");
+    tempDiv.dataset.id = id;
+    tempDiv.innerHTML = `
+            <div class="image">
+                <img src="${img}" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">${title}</a>
+                <span>${author}</span>
+                <p>${desc}</p>
+                <a href="#" class="btn">Читати більше</a>
+            </div>
+    `;
+
+    blogsList?.insertAdjacentElement("beforeend", tempDiv)
+})
+
+function submitHandler() {
+    setTimeout(() => {
+        document.getElementById("open-modal").classList.add("modal-window-open");
+    }, 300)
+}
+
+document.querySelector(".modal-close").addEventListener("click", () => {
+    document.getElementById("open-modal").classList.remove("modal-window-open");
+})
